8a0cd658fea13e7e31efb591f60c05860b1e5946
JJ <jj@oxibux.com>
2023-06-12 22:34:13 +0200

    feat: add description to coming soon section and center text

--- components/landing/comingSoon.tsx
+++ components/landing/comingSoon.tsx
@@ -6,8 +6,12 @@ export const ComingSoon = () => {
 
     return (
         <div>
-            <h2 className="text-6xl font-bold mb-24">Coming Soon</h2>
-            <GetNotifiedInput isExtra={true}/>
+            <div className="flex flex-col mb-24 items-center">
+                <h2 className="text-5xl sm:text-6xl md:text-6xl lg:text-6xl xl:text-6xl font-bold mb-5 text-center">Coming Soon</h2>
+                <p className="max-w-md text-center dark:text-zinc-400">Sign up to get notified when the dashboard builder
+                    application is available!</p>
+            </div>
+            <GetNotifiedInput isExtra={true} />
         </div>
     );
 };
